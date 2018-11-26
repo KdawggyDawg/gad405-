e8522118cb5567e0950d1c9392935414575c8ae2
KdawggyDawg <44429927+KdawggyDawg@users.noreply.github.com>
2018-11-26 18:23:32 +0000

    Shape Exercise Homework
    This should be the homework if it uploads properly, which it probably didn't, but hey ho, life's never easy am I right?

--- 00_Template/sketch.js
+++ 00_Template/sketch.js
@@ -2,22 +2,43 @@
 //00_Template
 //The purpose of this sketch is to provide a basic template
 //for P5.js projects
+var x, y;
 
-//Initialization function
 function setup() {
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Initialization: OK")
   createCanvas(500,500);
+  background(200, 160, 190);
+  frameRate(10);
+
 }
 
-//Rendering function
 function draw() {
-  //Set a background color
-  background(255, 0, 255);
-
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Rendering...")
-  noLoop();
+  //var s = random(200);
+  size = random(200);
+  customShape();
 }
+
+/*"var s", indicates random size of circles, s in the circle line references
+this so that it knows what size to change to!
+    */
+  function customShape(){
+    x = random(500)
+    y = random(500)
+      fill(random(255), 40, random(255), random(255));
+      rect(x, y, size, size);
+      stroke(10, 200, 220);
+      rect (x, y, size - 40, size - 40);
+      stroke(100, 100, 150);
+      fill(200, 255, 255);
+      rect (x, y, size - 80, size - 80);
+    }
+
+/*str cretaes lines around the circles, line 31 effects the fill of the
+innermost circle
+
+}
+//}
+
+/*fill(255);
+text("open the developer > Javascript Console", 10, 100);
+text("to see the nested loop number values", 10, 130);
+*/
